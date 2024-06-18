aa56461b609ff01f85b617f7b25adcf0dd18116e
TFAGaming <tayebfekih901@gmail.com>
2024-06-18 01:39:56 +0100

    Better update

--- js/demo.js
+++ js/demo.js
@@ -6,14 +6,19 @@
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#FFFFFF',
-    lineColor: '#FFFFFF'
+    dotColor: 'rgb(173, 173, 173, 0.3)',
+    lineColor: 'rgb(173, 173, 173, 0.3)'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
 
 console.warn('You can view the repository on GitHub instead of viewing the source of this site. https://github.com/TFAGaming/tfagaming.github.io');
+
+function show() {
+  document.getElementById('anotherFunction').classList.toggle('Active');
+}
+
 
 /*
 // jQuery plugin example:
